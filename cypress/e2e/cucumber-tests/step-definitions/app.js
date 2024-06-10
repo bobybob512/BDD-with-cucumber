372cdb079a7953e6461634466c24faab2269e154
Bob <>
2024-06-10 12:16:00 +0300

    steps definitions

--- cypress/e2e/cucumber-tests/step-definitions/app.js
+++ cypress/e2e/cucumber-tests/step-definitions/app.js
@@ -1,5 +1,16 @@
-import { defineStep } from 'cypress-cucumber-preprocessor/steps'
+import { Given, defineStep } from 'cypress-cucumber-preprocessor/steps'
 
 defineStep("I open example page", () => {
-    cy.visit('https://www.example.com')
-})
+    cy.visit('https://www.example.com');
+});
+
+defineStep("I see page title", () => {
+    cy.get("h1").should("be.visible");
+});
+
+defineStep("I see some text", () => {
+    cy.get("p").should("be.visible");
+});
+
+//it can be Given instead of defineStep and it will automatically be imported
+//Given("I open example page")
--- cypress/e2e/cucumber-tests/step-definitions/demo.feature
+++ cypress/e2e/cucumber-tests/step-definitions/demo.feature
@@ -5,5 +5,7 @@ Feature: : Demo Feature
 
     Scenario: Should load example website
         Given I open example page
-        #Then I see page title
-        #And I see some text
+        Then I see page title
+        And I see some text
+        #this is also possible
+        And I see some text 
